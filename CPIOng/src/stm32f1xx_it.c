391e92b5e265f4392af1aa29e768ed66561d8d9c
Broenne <marcus.broenstrup@gmail.com>
2018-11-27 06:07:51 +0100

    sss

--- CPIOng/src/stm32f1xx_it.c
+++ CPIOng/src/stm32f1xx_it.c
@@ -1,38 +1,38 @@
 /**
-  ******************************************************************************
-  * @file    stm32f1xx_it.c 
-  * @author  MCD Application Team
-  * @version V1.0.0
-  * @date    11-February-2014
-  * @brief   Main Interrupt Service Routines.
-  *          This file provides template for all exceptions handler and 
-  *          peripherals interrupt service routine.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    stm32f1xx_it.c
+ * @author  MCD Application Team
+ * @version V1.0.0
+ * @date    11-February-2014
+ * @brief   Main Interrupt Service Routines.
+ *          This file provides template for all exceptions handler and
+ *          peripherals interrupt service routine.
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+ *
+ * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *        http://www.st.com/software_license_agreement_liberty_v2
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************
+ */
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f1xx_it.h"
 
 /** @addtogroup IO_Toggle
-  * @{
-  */
+ * @{
+ */
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
@@ -41,67 +41,64 @@
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
 
-void SwitchMainLed(void){
-       TIM_ClearITPendingBit(TIM2, TIM_IT_Update); // setz timer zurück, achtung dann kann man ihn auch anders nicht mehr benutzen
-	  if(GPIO_ReadOutputDataBit(GPIOD, GPIO_Pin_2)){
-	    GPIO_WriteBit(GPIOD, GPIO_Pin_2, RESET);
-	  }else{
-	    GPIO_WriteBit(GPIOD, GPIO_Pin_2, SET);
-	  }
+void SwitchMainLed(void) {
+	TIM_ClearITPendingBit(TIM2, TIM_IT_Update); // setz timer zurück, achtung dann kann man ihn auch anders nicht mehr benutzen
+	if (GPIO_ReadOutputDataBit(GPIOD, GPIO_Pin_2)) {
+		GPIO_WriteBit(GPIOD, GPIO_Pin_2, RESET);
+	} else {
+		GPIO_WriteBit(GPIOD, GPIO_Pin_2, SET);
+	}
 }
 
-void SendCan(void){
-		static int i;
-		++i;
-		CanTxMsg canMessage;
-		canMessage.StdId = 0x123;
-		canMessage.ExtId = 0;
-		canMessage.RTR = CAN_RTR_DATA;
-		canMessage.IDE = CAN_ID_STD;
-		canMessage.DLC = 1;
-
-		canMessage.Data[0] = i;
-		while (!(CAN1->TSR & CAN_TSR_TME0 || CAN1->TSR & CAN_TSR_TME1 || CAN1->TSR & CAN_TSR_TME2)) {} // todo mb: das ding austimen lassen
-		CAN_Transmit(CAN2, &canMessage);
+void SanCanAlive() {
+	SendCan(0x123);
 }
 
+void SendCan(uint32_t id) {
+	static int i;
+	++i;
+	CanTxMsg canMessage;
+	canMessage.StdId = id;
+	canMessage.ExtId = 0;
+	canMessage.RTR = CAN_RTR_DATA;
+	canMessage.IDE = CAN_ID_STD;
+	canMessage.DLC = 1;
 
-void TIM2_IRQHandler(void){
+	canMessage.Data[0] = i;
+	while (!(CAN1->TSR & CAN_TSR_TME0 || CAN1->TSR & CAN_TSR_TME1
+			|| CAN1->TSR & CAN_TSR_TME2)) {
+	} // todo mb: das ding austimen lassen
+	CAN_Transmit(CAN2, &canMessage);
+}
+
+void TIM2_IRQHandler(void) {
 	SwitchMainLed();
-	SendCan();
+	SanCanAlive();
 }
 
-
-void EXTI0_IRQHandler(void){
-  printf("Rising edge on .... In 0\n");
-
-  EXTI_ClearITPendingBit(EXTI_Line0);
-  if(GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_9)){
-    GPIO_WriteBit(GPIOC, GPIO_Pin_9, RESET);
-  }else{
-    GPIO_WriteBit(GPIOC, GPIO_Pin_9, SET);
-  }
+void EXTI0_IRQHandler(void) {
+	printf("Rising edge on .... In 0\n");
+	EXTI_ClearITPendingBit(EXTI_Line0);
+	if (GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_9)) {
+		GPIO_WriteBit(GPIOC, GPIO_Pin_9, RESET);
+	} else {
+		GPIO_WriteBit(GPIOC, GPIO_Pin_9, SET);
+	}
 }
 
-
-
-void EXTI1_IRQHandler(void){
-  printf("Rising edge on .... In 1\n");
-
-  EXTI_ClearITPendingBit(EXTI_Line1);
-  if(GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_9)){
-    GPIO_WriteBit(GPIOC, GPIO_Pin_9, RESET);
-  }else{
-    GPIO_WriteBit(GPIOC, GPIO_Pin_9, SET);
-  }
+void EXTI1_IRQHandler(void) {
+	printf("Rising edge on .... In 1\n");
+	SendCan(0x01);
+	EXTI_ClearITPendingBit(EXTI_Line1);
+	if (GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_9)) {
+		GPIO_WriteBit(GPIOC, GPIO_Pin_9, RESET);
+	} else {
+		GPIO_WriteBit(GPIOC, GPIO_Pin_9, SET);
+	}
 }
-
-
 
 void CAN2_RX0_IRQHandler(void) {
-
 	printf("receive can 2 interrupt\n");
-
 	// todo mb: interrupts sperren
 	CanRxMsg RxMessage;
 	CAN_Receive(CAN2, CAN_FIFO0, &RxMessage);
@@ -112,107 +109,92 @@ void CAN2_RX0_IRQHandler(void) {
 	}
 }
 
-
-
 /******************************************************************************/
 /*            Cortex-M Processor Exceptions Handlers                          */
 /******************************************************************************/
 
 /**
-  * @brief  This function handles NMI exception.
-  * @param  None
-  * @retval None
-  */
-void NMI_Handler(void)
-{
+ * @brief  This function handles NMI exception.
+ * @param  None
+ * @retval None
+ */
+void NMI_Handler(void) {
 }
 
 /**
-  * @brief  This function handles Hard Fault exception.
-  * @param  None
-  * @retval None
-  */
-void HardFault_Handler(void)
-{
-  /* Go to infinite loop when Hard Fault exception occurs */
-  while (1)
-  {
-  }
+ * @brief  This function handles Hard Fault exception.
+ * @param  None
+ * @retval None
+ */
+void HardFault_Handler(void) {
+	/* Go to infinite loop when Hard Fault exception occurs */
+	while (1) {
+	}
 }
 
 /**
-  * @brief  This function handles Memory Manage exception.
-  * @param  None
-  * @retval None
-  */
-void MemManage_Handler(void)
-{
-  /* Go to infinite loop when Memory Manage exception occurs */
-  while (1)
-  {
-  }
+ * @brief  This function handles Memory Manage exception.
+ * @param  None
+ * @retval None
+ */
+void MemManage_Handler(void) {
+	/* Go to infinite loop when Memory Manage exception occurs */
+	while (1) {
+	}
 }
 
 /**
-  * @brief  This function handles Bus Fault exception.
-  * @param  None
-  * @retval None
-  */
-void BusFault_Handler(void)
-{
-  /* Go to infinite loop when Bus Fault exception occurs */
-  while (1)
-  {
-  }
+ * @brief  This function handles Bus Fault exception.
+ * @param  None
+ * @retval None
+ */
+void BusFault_Handler(void) {
+	/* Go to infinite loop when Bus Fault exception occurs */
+	while (1) {
+	}
 }
 
 /**
-  * @brief  This function handles Usage Fault exception.
-  * @param  None
-  * @retval None
-  */
-void UsageFault_Handler(void)
-{
-  /* Go to infinite loop when Usage Fault exception occurs */
-  while (1)
-  {
-  }
+ * @brief  This function handles Usage Fault exception.
+ * @param  None
+ * @retval None
+ */
+void UsageFault_Handler(void) {
+	/* Go to infinite loop when Usage Fault exception occurs */
+	while (1) {
+	}
 }
 
 /**
-  * @brief  This function handles SVCall exception.
-  * @param  None
-  * @retval None
-  */
-void SVC_Handler(void)
-{
+ * @brief  This function handles SVCall exception.
+ * @param  None
+ * @retval None
+ */
+void SVC_Handler(void) {
 }
 
 /**
-  * @brief  This function handles Debug Monitor exception.
-  * @param  None
-  * @retval None
-  */
-void DebugMon_Handler(void)
-{
+ * @brief  This function handles Debug Monitor exception.
+ * @param  None
+ * @retval None
+ */
+void DebugMon_Handler(void) {
 }
 
 /**
-  * @brief  This function handles PendSVC exception.
-  * @param  None
-  * @retval None
-  */
-void PendSV_Handler(void)
-{
+ * @brief  This function handles PendSVC exception.
+ * @param  None
+ * @retval None
+ */
+void PendSV_Handler(void) {
 }
 
 /**
-  * @brief  This function handles SysTick Handler.
-  * @param  None
-  * @retval None
-  */
-void SysTick_Handler(void)
-{
+ * @brief  This function handles SysTick Handler.
+ * @param  None
+ * @retval None
+ */
+void SysTick_Handler(void) {
 }
 
 /******************************************************************************/
@@ -223,17 +205,16 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
-  * @brief  This function handles PPP interrupt request.
-  * @param  None
-  * @retval None
-  */
+ * @brief  This function handles PPP interrupt request.
+ * @param  None
+ * @retval None
+ */
 /*void PPP_IRQHandler(void)
-{
-}*/
+ {
+ }*/
 
 /**
-  * @}
-  */ 
-
+ * @}
+ */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
