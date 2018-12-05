27b30deffb663733498e4b36e4177bfc18538939
Broenne <marcus.broenstrup@gmail.com>
2018-12-05 05:55:18 +0100

    aa

---
+++ CPIOng/src/IO.h
@@ -0,0 +1,19 @@
+/*
+ * IO.h
+ *
+ *  Created on: 05.12.2018
+ *      Author: MB
+ */
+
+#ifndef IO_H_
+#define IO_H_
+
+#include "main.h"
+
+uint8_t ReadInputsFromRegisterA(void);
+uint8_t ReadInputsFromRegisterB(void);
+uint8_t ReadInputsFromRegisterC(void);
+void GetInputs(uint8_t* data);
+
+
+#endif /* IO_H_ */
