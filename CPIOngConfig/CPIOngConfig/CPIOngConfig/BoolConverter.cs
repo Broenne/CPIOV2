95182c693992414276d19e3708e9fce01e6b5b75
Broenne <marcus.broenstrup@gmail.com>
2019-02-06 06:02:44 +0100

    enable dingsbums

---
+++ CPIOngConfig/CPIOngConfig/CPIOngConfig/BoolConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace CPIOngConfig
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class BoolConverter : IValueConverter
+    {
+        #region Public Methods
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = (bool)value;
+            return !v;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
