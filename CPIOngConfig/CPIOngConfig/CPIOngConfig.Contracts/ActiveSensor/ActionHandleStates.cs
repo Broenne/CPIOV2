5c81211d117c858701ceac4769b2abb69ba0d3ca
Broenne <marcus.broenstrup@gmail.com>
2019-06-08 20:47:14 +0200

    www

--- CPIOngConfig/CPIOngConfig/CPIOngConfig.Contracts/ActiveSensor/ActionHandleStates.cs
+++ CPIOngConfig/CPIOngConfig/CPIOngConfig.Contracts/ActiveSensor/ActionHandleStates.cs
@@ -6,10 +6,46 @@ namespace ConfigLogicLayer.DigitalInputState
 {
     public enum ActionHandleStates
     {
-        Namur = 0,
-        Reed = 1,
-        Licht = 2,
-        Reset = 3,
-        Impulse = 4
+        // todo mb: vorbeitung für martin
+        //Namur = 0,
+        //Reed = 1,
+        //Licht = 2,
+        //Reset = 3,
+        //Impulse = 4
+
+        /// <summary>
+        /// The none status.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The read info.
+        /// </summary>
+        Read = 1,
+
+        /// <summary>
+        /// The namur.
+        /// </summary>
+        Namur = 2,
+
+        /// <summary>
+        /// The licht.
+        /// </summary>
+        Licht = 3,
+
+        /// <summary>
+        /// The QMIN info.
+        /// </summary>
+        Qmin = 4,
+
+        /// <summary>
+        /// The QMAX info.
+        /// </summary>
+        Qmax = 5,
+
+        /// <summary>
+        /// The analog.
+        /// </summary>
+        Analog = 6
     }
 }
