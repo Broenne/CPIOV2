2c62dd9aeabc8462a08ec9b5fdfd77528b92a798
Broenne <marcus.broenstrup@gmail.com>
2019-06-08 19:03:08 +0200

    rrr

---
+++ CPIOngConfig/CPIOngConfig/CPIOngConfig.Contracts/ActiveSensor/ActionHandleStates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfigLogicLayer.DigitalInputState
+{
+    public enum ActionHandleStates
+    {
+        Namur = 0,
+        Reed = 1,
+        Licht = 2,
+        Reset = 3,
+        Impulse = 4
+    }
+}
